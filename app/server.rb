65fd7a1c99baf584a5a86f711d8948ff361b1158
Pavel Redics <pavel.redics@gmail.com>
2015-01-20 10:41:30 +0000

    gyroscope working, set compass page

--- app/server.rb
+++ app/server.rb
@@ -3,9 +3,17 @@ require 'sinatra'
 
 class LookUp < Sinatra::Base
 
+  # set :views, Proc.new{File.join(root, "views")}
+  set :public_dir, File.join(root, '..', 'public')
 
   get '/' do
     erb :index
   end
+
+  get '/compass' do
+    erb :compass
+  end
+
+  run! if app_file == $0
 
 end
