d5fa8ddfe9a0c0b0dc9fe05cf1467280385e7e84
Richard Lake <ralake1985@gmail.com>
2015-01-20 17:37:49 +0000

    Add shading page and first test for content

--- features/step_definitions/shading_steps.rb
+++ features/step_definitions/shading_steps.rb
@@ -1,5 +1,7 @@
 Given(/^I am on shading page$/) do
-  pending # express the regexp above with the code you wish you had
+  visit '/shading'
+  expect(page).to have_content("Shading")
+  expect(page).to have_content("Use the slider to estimate how much shade")
 end
 
 When(/^I choose shade value$/) do
