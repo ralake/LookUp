08842bd3fde9b8e537d366544c3bfe3c682e5322
Tr1ckX <attila.mirk@gmail.com>
2015-01-21 09:30:17 +0000

    greyout works on phone

--- app/public/js/greyBox.js
+++
@@ -1,3 +0,0 @@
-function greyOut() { $('#screen').css( { "width": $(document).width(), "height": $(document).height() })
-	.click(function() { $(this).css("display", "none"); $('#screen').css("display", "none"); });
-}
