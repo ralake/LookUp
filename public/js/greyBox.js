cfbff376271788b7eaa98aa03622b548a5fa14ef
Tr1ckX <attila.mirk@gmail.com>
2015-01-21 17:46:29 +0000

    one press cam launch

--- public/js/greyBox.js
+++ public/js/greyBox.js
@@ -1,3 +1,6 @@
 function greyOut(styleToApply) { $(styleToApply).css( { "width": $(document).width(), "height": $(document).height() })
-	.click(function() { $(this).css("display", "none"); $(styleToApply).css("display", "none"); });
+	.click(function() {
+		 	$(this).css("display", "none"); $(styleToApply).css("display", "none");
+			$('#takePictureField').click(); 
+		});
 }
