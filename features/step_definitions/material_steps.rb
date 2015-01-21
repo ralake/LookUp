3898141d4c84932a4a29796c14c355112cbd28e6
Pavel Redics <pavel.redics@gmail.com>
2015-01-21 18:06:36 +0000

    choosing roof material passes

--- features/step_definitions/material_steps.rb
+++ features/step_definitions/material_steps.rb
@@ -4,7 +4,7 @@ Given(/^I am on the material page$/) do
 end
 
 When(/^I choose a roof material$/) do
-  find_button('materials').value('tiles').click
+  find("input[value='tiles']").click
   click_on "Next"
 end
 
