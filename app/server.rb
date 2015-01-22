f57b4b4c6e04a9c2333d851dc87dbd2ed30c74a4
Richard Lake <ralake1985@gmail.com>
2015-01-22 16:24:31 +0000

    Ready to implement gyroscope

--- app/server.rb
+++ app/server.rb
@@ -2,7 +2,6 @@ require 'sinatra'
 require 'data_mapper'
 require_relative 'models/roof'
 require_relative 'data_mapper_setup' 
-
 require_relative 'controllers/angle' 
 require_relative 'controllers/index' 
 require_relative 'controllers/infopage' 
@@ -11,13 +10,15 @@ require_relative 'controllers/photo'
 require_relative 'controllers/shading' 
 require_relative 'controllers/summary' 
 
-
-
 class LookUp < Sinatra::Base
 
   set :views, File.join(root, '..', 'views')
-  set :public_dir, File.join(root, '..', 'public')
+  set :public_dir, File.join(root, '../..', 'public')
   enable :sessions
+
+  get '/gyroscope' do
+    erb :gyroscope
+  end
 
   run! if app_file == $0
 
