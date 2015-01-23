20c886d73bffb7738d4641801ec3f6abf7f7e12d
Richard Lake <ralake1985@gmail.com>
2015-01-23 16:16:12 +0000

    Tests passing, ready for development merge

--- app/controllers/angle.rb
+++ app/controllers/angle.rb
@@ -5,8 +5,7 @@ class LookUp < Sinatra::Base
   end
 
   post '/roof_type' do
-    p session
-    roof = Roof.first_or_create(:id => session[:roof_id])
+    roof = Roof.first(:id => session[:roof_id])
     if params[:roof_type] == "Flat"
       roof.update(roof_angle: 0)
       redirect to '/summary'
@@ -20,7 +19,7 @@ class LookUp < Sinatra::Base
   end
 
   post '/sloped_roof_angle' do
-    roof = Roof.first_or_create(:id => session[:roof_id])
+    roof = Roof.first(:id => session[:roof_id])
     roof.update(roof_angle: params[:roof_angle].to_i)
     redirect to '/summary'
   end 
--- app/controllers/shading.rb
+++ app/controllers/shading.rb
@@ -5,8 +5,9 @@ class LookUp < Sinatra::Base
   end
 
   post '/shading_data' do
-    roof = Roof.first_or_create(:id => session[:roof_id])
+    roof = Roof.first(:id => session[:roof_id])
     roof.update(shade_value: params[:shade_value].to_i)
+    p roof
     redirect to '/roof_angle'
   end
 
