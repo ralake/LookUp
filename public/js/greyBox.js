9b756fbe059bdf4922e179e5558d6216d5041a2c
Shan Hasan <shan119@hotmail.co.uk>
2015-01-24 17:13:13 +0000

    updated greybox and photo js pages to allow pic taking

--- public/js/greyBox.js
+++ public/js/greyBox.js
@@ -1,8 +1,8 @@
 function greyOut(styleToApply) { $(styleToApply).css( { "width": $(document).width(), "height": $(document).height() })
   .click(function() {
       $(this).css("display", "none"); $(styleToApply).css("display", "none");
-      $("#submit_button").fadeIn();
       $('#takePictureField').click();
+      $("#submit_button").fadeIn();
       $("#yourimage").fadeIn();
       $("#takePictureField").on("change",gotPic); 
     });
