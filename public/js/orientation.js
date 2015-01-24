f2e15c1f2ddefc0d19466f70c868f0150f763b63
Pavel Redics <pavel.redics@gmail.com>
2015-01-24 16:11:33 +0000

    Added explanation for roof angle page

--- public/js/orientation.js
+++ public/js/orientation.js
@@ -4,26 +4,20 @@ function orientation() {
     if(window.DeviceOrientationEvent) {
 
       window.addEventListener('deviceorientation', function(event) {
-        // var alpha;
-        //Check for iOS property
         if(event.webkitCompassHeading) {
           alpha = Math.round(event.webkitCompassHeading);
-          //Rotation is reversed for iOS
           compass.style.WebkitTransform = 'rotate(-' + alpha + 'deg)';
         }
-        //non iOS
         else {
           alpha = Math.round(event.alpha);
           webkitAlpha = alpha;
           if(!window.chrome) {
-            //Assume Android stock (this is crude, but good enough for our example) and apply offset
             webkitAlpha = alpha-270;
           }
         }
 
         compass.style.Transform = 'rotate(' + alpha + 'deg)';
         compass.style.WebkitTransform = 'rotate('+ webkitAlpha + 'deg)';
-        //Rotation is reversed for FF
         compass.style.MozTransform = 'rotate(-' + alpha + 'deg)'; 
       }, false);
     }
