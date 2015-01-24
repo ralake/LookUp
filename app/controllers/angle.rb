f5604a561a835eaf956509bfcd09e5a86893f67d
Pavel Redics <pavel.redics@gmail.com>
2015-01-24 15:47:12 +0000

    refactor angle controller, roof_facing, set_azimuth methods

--- app/controllers/angle.rb
+++ app/controllers/angle.rb
@@ -1,19 +1,27 @@
 class LookUp < Sinatra::Base
+
+  def set_azimuth(params)
+    compass = params.to_i
+    return compass-180 if compass > 180
+    return compass+180 if compass < 180
+  end
  
+
+  def roof_facing(azimuth)
+    if [*(271...360), *(0...90)].include?(azimuth)
+      flash[:notice] = "Sorry you roof facing north" 
+      redirect to '/'
+    end
+  end
+
   get '/roof_angle' do
     erb :roof_angle
   end
 
   post '/flat_roof_orientation_angle' do
-    compass, orient = params[:flat_orientation].to_i, 0
-    compass > 180 ? orient = compass-180 : orient = compass+180
-    if [*(271...360), *(0...90)].include?(orient)
-      flash[:notice] = "Sorry you roof facing north" 
-      redirect to '/'
-    end
+    roof_facing(set_azimuth(params[:flat_orientation]))
     roof = Roof.first(:id => session[:roof_id])
-    roof.update(roof_angle: 0)
-    roof.update(orientation: params[:flat_orientation])
+    roof.update(roof_angle: 0, orientation: set_azimuth(params[:flat_orientation]))
     redirect to '/summary'
   end
 
@@ -22,8 +30,9 @@ class LookUp < Sinatra::Base
   end
 
   post '/sloped_roof_orientation' do
+    roof_facing(set_azimuth(params[:sloped_orientation]))
     roof = Roof.first(:id => session[:roof_id])
-    roof.update(orientation: params[:sloped_orientation])
+    roof.update(orientation: set_azimuth(params[:sloped_orientation]))
     redirect to '/sloped_roof'
   end
 
