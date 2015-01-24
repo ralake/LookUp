6a90f2eb54790117c663cc2f3f81668310f97e5a
Shan Hasan <shan119@hotmail.co.uk>
2015-01-24 12:29:47 +0000

    updated CSS info button on all pages, also added jQuery to photo page to improve the appearance

--- public/js/greyBox.js
+++ public/js/greyBox.js
@@ -1,6 +1,7 @@
 function greyOut(styleToApply) { $(styleToApply).css( { "width": $(document).width(), "height": $(document).height() })
   .click(function() {
       $(this).css("display", "none"); $(styleToApply).css("display", "none");
+      $("#submit_button").fadeIn();
       $('#takePictureField').click(); 
     });
 }
