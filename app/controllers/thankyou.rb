cd948038d662e978439fc8e9783cde21e0b91b40
Richard Lake <ralake1985@gmail.com>
2015-01-26 11:21:43 +0000

    Extract mailgun code into helper method to call in controller

--- app/controllers/thankyou.rb
+++ app/controllers/thankyou.rb
@@ -8,16 +8,7 @@ class LookUp < Sinatra::Base
   post '/useremail' do
     roof = Roof.find_roof(session[:roof_id])
     roof.add_user_email(params[:email])
-    mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
-    message_params = {from: ENV['MAILGUN_FROM_ADDRESS'], 
-                      to: "#{roof.user_email}",
-                      subject: "Lookup.solar link",
-                      text: "Hi #{roof.discovered_by}!\n
-                            Please follow the link below on a desktop computer to complete the process.\n
-                            https://lookupdev.herokuapp.com?id=#{roof.id}"
-    }
-    mg_client.send_message "#{ENV['MAILGUN_SANDBAG']}", message_params
-    puts mg_client
+    send_email_with_link(roof)
     redirect to 'http://www.1010global.org/uk'
   end
 
