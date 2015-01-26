edd2676baa83dc8d56f3a5e2f54bc51809b62220
Richard Lake <ralake1985@gmail.com>
2015-01-26 11:17:58 +0000

    Add mailgun to /useremail post

--- app/controllers/thankyou.rb
+++ app/controllers/thankyou.rb
@@ -6,16 +6,18 @@ class LookUp < Sinatra::Base
   end
 
   post '/useremail' do
-    roof = Roof.find_roof(session[:roof_id]).add_user_email(params[:email])
+    roof = Roof.find_roof(session[:roof_id])
+    roof.add_user_email(params[:email])
     mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
     message_params = {from: ENV['MAILGUN_FROM_ADDRESS'], 
-                      to: roof.email,
+                      to: "#{roof.user_email}",
                       subject: "Lookup.solar link",
                       text: "Hi #{roof.discovered_by}!\n
                             Please follow the link below on a desktop computer to complete the process.\n
                             https://lookupdev.herokuapp.com?id=#{roof.id}"
     }
-    mg_client.send_message 'sandbox9533715787bf49408a576e6f77424cf3.mailgun.org', message_params
+    mg_client.send_message "#{ENV['MAILGUN_SANDBAG']}", message_params
+    puts mg_client
     redirect to 'http://www.1010global.org/uk'
   end
 
