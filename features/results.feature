b46c4fcd0744effa970bc448e5126ef087523587
Pavel Redics <pavel.redics@gmail.com>
2015-01-28 18:04:37 +0000

    writing feature test for results page

--- features/results.feature
+++ features/results.feature
@@ -1,3 +1,10 @@
 Feature: Results
   In order to get capacity
-  
+  When I've completed the steps
+  I should see the outcome
+
+
+  Scenario: Results
+    Given That there is a roof and I on results page
+    Then I should see estimates fo solar panel and power capacity
+    And then should see 10:10 website
---
+++ features/step_definitions/results_steps.rb
@@ -0,0 +1,12 @@
+Given(/^That there is a roof and I on results page$/) do
+  @roof
+end
+
+Then(/^I should see estimates fo solar panel and power capacity$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^then should see (\d+):(\d+) website$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
