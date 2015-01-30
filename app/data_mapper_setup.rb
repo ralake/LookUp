ee2ba2e4419573241f422949de7d4b0e57d94a57
effisfor <tom@thetuttroom.com>
2015-01-30 14:07:00 +0000

    Remove local lookup db role

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,6 +1,6 @@
 env = ENV["RACK_ENV"] || "development"
 
-DataMapper.setup(:default, ENV["DATABASE_URL"] || "postgres://postgres:a@localhost/look_up_#{env}")
+DataMapper.setup(:default, ENV["DATABASE_URL"] || "postgres://localhost/look_up_#{env}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
