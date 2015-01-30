5055b9f63f30fa09473c65f74cac8d43a3ce881b
effisfor <tom@thetuttroom.com>
2015-01-30 11:53:55 +0000

    Update slider/shade page

--- public/js/lookup.js
+++ public/js/lookup.js
@@ -8,4 +8,22 @@ $(document).ready(function() {
     $('#material-input').val($.trim($(this).text()));
   });
   
+  // Update shade percent on slider change
+  $('#shader').change(function() {
+    $('#shade').text($(this).val() + "%");
+  });
+  
+  var shader_interval;
+  
+  $('body').on("touchstart", "#shader", function() {
+    var el = $(this);
+    shader_interval = setInterval(function() {
+      $('#shade').text(el.val() + "%");
+    }, 50);
+  });
+
+  $('body').on("touchend", "#shader", function() {
+    clearInterval(shader_interval);
+  })
+  
 });
