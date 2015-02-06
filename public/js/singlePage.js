15c750b86ddaf6f0fb11ef10494011f2e1d54c83
Richard Lake <ralake1985@gmail.com>
2015-02-06 09:42:51 +0000

    Ready to add in AJAX requests

--- public/js/singlePage.js
+++ public/js/singlePage.js
@@ -4,11 +4,12 @@ function show(elementID) {
     alert("no such element");
     return;
   }
-
+  
   var pages = document.getElementsByClassName('page');
   for(var i = 0; i < pages.length; i++) {
     pages[i].style.display = 'none';
   }
 
   element.style.display = 'block';
+
 }
