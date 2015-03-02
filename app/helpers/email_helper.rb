6cf14123abed94d18a2a5f231ea3cacc502c3ca8
Richard Lake <ralake1985@gmail.com>
2015-03-02 19:09:09 +0000

    Different email bodies and subjects being sent to repeat users. Ready for heroku test

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,9 +1,9 @@
-def send_email_with_link(roof)
+def send_user_email(roof)
   mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
   message_params = {from: ENV['MAILGUN_FROM_ADDRESS'], 
                     to: "#{roof.user_email}",
-                    subject: "Lookup.solar Results",
-                    text: email_body(roof)
+                    subject: subject_text(roof),
+                    text: body_text(roof)
   }
   mg_client.send_message "#{ENV['MAILGUN_SANDBAG']}", message_params
 end
@@ -11,19 +11,51 @@ end
 def send_feedback_email(msg)
   mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
   message_params = {from: ENV['MAILGUN_FROM_ADDRESS'], 
-                    to: ENV['MAILGUN_FROM_ADDRESS'],
+                    to: 'ralake1985@gmail.com',
                     subject: "Lookup.solar Feedback",
                     text: msg
   }
   mg_client.send_message "#{ENV['MAILGUN_SANDBAG']}", message_params
 end
 
-def email_body(roof)
-  "Hi #{roof.discovered_by}!\n
-  Based on the information that we gathered during your survey,\n
-  we think that the roof can hold #{roof.panel_capacity} panels.\n
-  This could generate an estimated power output of #{roof.power_capacity}kw.\n
-  Please give us a call at the 10:10 office to find out more about the next steps!"
+def subject_text(roof)
+  if first_time_user?(roof.user_email)
+    return "Welcome to Look Up! Here’s your first report."
+  else
+    "Here's your latest Look Up discovery!"
+  end
+end
+
+def body_text(roof)
+  if first_time_user?(roof.user_email)
+    first_time_user_email_body(roof)
+  else
+    repeat_user_email_body(roof)
+  end
+end
+
+def first_time_user_email_body(roof)
+  "Hi #{roof.user_email}, congratulations on your first Look Up discovery!\n 
+  Here’s the full report – you should probably print it out and frame it or something.\n
+  - #{roof.panel_capacity} panels should fit on the roof.\n
+  - This could generate #{roof.power_capacity}, enough to power #{roof.homes_to_power} homes!\n
+  What next?\n
+  You've found a good-looking roof - now let's put some solar panels on it!\n
+  If there's a community energy group nearby, they'll be able to see your report, and maybe host their next solar rig on the roof you found!\n
+  No groups near you? Get tips on starting one here: xxxx
+  Not your thing? Keep snapping those roofs anyway. The more there are in the database, the easier it'll be for someone else to get something going later on.\n
+  10:10 Team"
+end
+
+def repeat_user_email_body(roof)
+  "Hi #{roof.user_email},\n
+  Looks like you've found another promising solar roof on Look Up. Nice work!\n
+  Here's your report:
+  - #{roof.panel_capacity} panels should fit on the roof.\n
+  - This should generate #{roof.power_capacity}, enough to power #{roof.homes_to_power} homes!\n
+  You can see your other discoveries at lookup.solar.\n
+  Keep hunting!\n
+  The Look Up team"
 end
 
 def email_validation(roof)
@@ -33,10 +65,14 @@ def email_validation(roof)
 end
 
 def valid_email(roof)
-  send_email_with_link(roof)
+  send_user_email(roof)
   roof.to_json 
 end
 
 def invalid_email(roof)
   {"errors" => roof.errors.full_messages}.to_json
+end
+
+def first_time_user?(email)
+  Roof.all(user_email: email).length == 1
 end
